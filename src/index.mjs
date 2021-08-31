0c35e3a80db361626dd56f58253be6933bb4ba1d
mjkt39 <k.miyaji@swallowarc.jp>
2021-08-31 10:58:05 +0900

    add s3 client

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,16 @@
 import {OutlineClient} from "./outline.mjs";
 import {Env} from "./env.mjs";
 import {Backuper} from "./backuper.mjs";
+import {S3Client} from "./s3.js";
 
 const env = new Env();
-const oc = new OutlineClient(env.outline.baseURL, env.outline.token)
+const oc = new OutlineClient(env.outline.baseURL, env.outline.token);
+const s3 = new S3Client(env.aws.region, env.aws.s3Bucket);
 const invoker = new Backuper(oc, null);
 
-env.outline.collectionIDs.forEach(id => {
-  invoker.Invoke(id).catch(reason => {
-    console.error(reason);
-  });
-});
+// env.outline.collectionIDs.forEach(id => {
+//   invoker.Invoke(id).catch(reason => {
+//     console.error(reason);
+//   });
+// });
 
--- src/s3.js
+++ src/s3.js
@@ -1,13 +1,52 @@
 import aws from 'aws-sdk';
 
 export class S3Client {
-  constructor(region, bucket) {
+  #bucket;
+  #s3;
+
+  constructor(region, bucketName) {
     // see https://docs.aws.amazon.com/ja_jp/sdk-for-javascript/v2/developer-guide/s3-example-creating-buckets.html
     aws.config.update({region: region});
-    this.s3 = new aws.S3({apiVersion: '2006-03-01'});
+    this.#s3 = new aws.S3({apiVersion: '2006-03-01'});
+    this.#bucket = bucketName;
   }
 
-  async Upload() {
+  async ListBuckets() {
+    let result = null;
+    this.#s3.listBuckets(function (err, data) {
+      if (err) {
+        throw err;
+      }
 
+      result = data.Buckets;
+    });
+
+    return result;
+  }
+
+  async Upload(key, data) {
+    const uploadParams = {Bucket: this.#bucket, Key: key, Body: data};
+    this.#s3.upload(uploadParams, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      if (data) {
+        console.log("Upload Success", data.Location);
+      }
+    });
+  }
+
+  async Delete(key) {
+    const deleteParams = {
+      Bucket: this.#bucket,
+      Key: key,
+    };
+    this.#s3.deleteObject(deleteParams, function (err, data) {
+      if (err) {
+        throw err;
+      }
+
+      console.log("Delete Success", data);
+    });
   }
 }
